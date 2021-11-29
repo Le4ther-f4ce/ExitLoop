9946f201d135435c9e856aa8296578350a0b35ea
Алексей Федюшин <alexeyfedyshinural@gmail.com>
2021-11-29 14:33:52 +0500

    Добавьте файлы проекта.

---
+++ ExitLoop/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace ExitLoop
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                Console.Write("Введите команду: ");
+                string userInput = Console.ReadLine();
+
+                if(userInput.ToLower() == "exit")
+                {
+                    break;
+                }
+                else
+                {
+                    Console.WriteLine($"Вы ввели - \"{userInput}\", пожалуйста введите exit");
+                }
+            }
+        }
+    }
+}
